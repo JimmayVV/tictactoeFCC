94ea41ab106f262961ace0e112e10c7eee521374
monicaleep <monica.parrillo@gmail.com>
2018-03-21 21:09:45 -0500

    elementary computer logic

--- src/game.js
+++ src/game.js
@@ -6,7 +6,8 @@ var Game = (function() {
     gameOver: false,
     userPlayer: "",
     computerPlayer: "",
-    numMoves: 0
+    numMoves: 0,
+    availableMoves: []
   };
 
   const WINS = [
@@ -23,18 +24,26 @@ var Game = (function() {
   function setUserPlayer(str) {
     STATUS.userPlayer = str;
     STATUS.computerPlayer = str == "X" ? "O" : "X";
-    STATUS.isX = str == "X" ? true : false;
+    // STATUS.isX = str == "X" ? true : false; would need this line for a 2 player mode
     render();
+    if (STATUS.userPlayer=="O"){
+      console.log(STATUS.isX)
+      computerMove();
+    }
+
   }
 
   function render() {
+    DOM.boxes = [];
     for (let i = 1; i < 10; i++) {
       var box = document.createElement("div");
       box.classList.add('box');
-      box.id = i;
+      box.setAttribute("data-value",i);
       DOM.game.appendChild(box);
+      DOM.boxes.push(box);
     }
     DOM.game.style.display = "flex";
+
   }
 
   function cacheDom() {
@@ -48,13 +57,12 @@ var Game = (function() {
     if (STATUS.gameOver) {
       console.log(STATUS.winner + " is the winner");
     }
-
   }
 
   function checkWin(winArray) {
     let result = "";
     winArray.forEach(function(item) {
-      result += document.getElementById(item).innerHTML;
+      result += document.querySelector('[data-value="'+item+'"]').innerHTML;
     })
     if (result == "XXX" || result == "OOO") {
       STATUS.winner = result[0];
@@ -64,7 +72,7 @@ var Game = (function() {
   }
 
   function play() {
-    DOM.game.addEventListener("click",  humanMove)
+    DOM.game.addEventListener("click",  handleClick)
   }
 
   function checkDraw(){
@@ -73,7 +81,7 @@ var Game = (function() {
     }
   }
 
-  function humanMove(e){
+  function handleClick(e){
     var target = e.target;
     if (target.innerHTML == "" && !STATUS.gameOver) {
       target.innerHTML = STATUS.isX ? "X" : "O";
@@ -82,10 +90,32 @@ var Game = (function() {
       STATUS.numMoves++
       if (!STATUS.gameOver){
         checkDraw();
-      
+        computerMove();
       }
     }
+  }
 
+  function getEmptySquares(){
+   STATUS.availableMoves = [];
+    DOM.boxes.forEach(function addAvailable(box){
+      if (box.innerHTML==""){
+        STATUS.availableMoves.push(box.getAttribute('data-value'))
+      }
+    })
+
+  }
+
+  function computerMove(){
+    getEmptySquares();
+    let place = STATUS.availableMoves[Math.floor(Math.random()*STATUS.availableMoves.length)]
+    let target = DOM.game.querySelector('[data-value="'+place+'"]');
+    target.innerHTML = STATUS.isX?"X":"O";
+    STATUS.isX = !STATUS.isX
+    STATUS.numMoves++
+    checkWins()
+    if (!STATUS.gameOver){
+      checkDraw()
+    }
   }
 
 
